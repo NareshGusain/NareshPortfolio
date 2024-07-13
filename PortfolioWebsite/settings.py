dcc8ca4bfe98c2dda1b87da7b79032b8576f9025
Naresh Gusain <nareshgusain00@gmail.com>
2024-07-13 10:14:06 +0530

    static url added

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
@@ -127,6 +128,7 @@ MEDIA_URL = 'media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
